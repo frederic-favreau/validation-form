f62aafbfdccf47036a9b739019b3bccd5ebd68d5
Frédéric Favreau <frederic.favreau.dev@gmail.com>
2023-08-31 23:39:12 +0200

    condition in progress

--- validation-form.js
+++ validation-form.js
@@ -1,24 +1,75 @@
-const submitButton = document.getElementsByClassName("submitButton");
-
-submitButton[0].addEventListener("click", function (e) {
+// Événement de soumission du formulaire
+document.querySelector("form").addEventListener("click", function (e) {
   e.preventDefault();
 });
 
-// vérifier la longeur de la valeur du champ username
-const userInput = document.getElementById("username");
-const inputBorder = document.querySelector("input");
-const rowForm = document.querySelector(".row");
+// Fonctions communes
+function createAlertDiv(row) {
+  const alertDiv = document.createElement("div");
+  alertDiv.classList.add("box-alert");
+  row.append(alertDiv);
+  return alertDiv;
+}
 
-
-userInput.addEventListener("keyup", function (e) {
-  if (e.target.value.length > 20) {
-    const alertDiv = document.createElement("div");
-    alertDiv.classList.add("box-alert");
-    alertDiv.innerHTML =
-      "Le nom d'utilisateur ne doit pas dépasser 20 caractères";
-    rowForm.appendChild(alertDiv);
-    inputBorder.style.backgroundColor = "red";
-  } else {
-    inputBorder.style.backgroundColor = "green";
+function removeAlertDiv(row) {
+  const alertDiv = row.querySelector(".box-alert");
+  if (alertDiv) {
+    row.removeChild(alertDiv);
   }
+}
+
+function validateInput(input, condition, message, row) {
+  removeAlertDiv(row);
+  if (condition(input.value)) {
+    const alertDiv = createAlertDiv(row);
+    alertDiv.innerHTML = message;
+    input.classList.add("invalid");
+  } else {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  }
+}
+
+// Écouteurs d'événement et validations spécifiques
+document.getElementById("username").addEventListener("keyup", function (e) {
+  const row = e.target.closest(".row");
+  validateInput(
+    e.target,
+    (value) => value.length < 3 || value.length > 20,
+    "Le nom d'utilisateur doit contenir entre 3 et 20 caractères",
+    row
+  );
+});
+
+document.getElementById("mail").addEventListener("keyup", function (e) {
+  const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const row = e.target.closest(".row");
+  validateInput(
+    e.target,
+    (value) => !regexMail.test(value),
+    "Veuillez entrer un mail valide",
+    row
+  );
+});
+
+document.getElementById("password").addEventListener("keyup", function (e) {
+  const row = e.target.closest(".row");
+  // le mot de passe doit : faire au moins 8 caractères, avoir au moins 1 majuscule, avoir au moins 1 caractère spécial
+  validateInput(
+    e.target,
+    (value) =>
+      value.length < 8 || !/[A-Z]/.test(value) || !/[!@#$%^&*]/.test(value),
+    "Le mot de passe doit contenir au moins 8 caractères, une majuscule et un caractère spécial",
+    row
+  );
+});
+
+document.getElementById("password2").addEventListener("keyup", function (e) {
+  const row = e.target.closest(".row");
+  validateInput(
+    e.target,
+    (value) => value !== document.getElementById("password").value,
+    "Les mots de passe ne correspondent pas",
+    row
+  );
 });
